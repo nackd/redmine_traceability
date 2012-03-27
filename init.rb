105b3677029f44df3731c1ba36337b98747bbf73
Jose M. Prieto <jmprieto@emergya.com>
2012-03-27 12:17:03 +0200

    Translate the plugin info in a different way
    Do not override the admin/plugins view.
    Translate the plugin info acording to default language setting.
    Register in an after_initialize block, so translation files are already
    loaded.

--- init.rb
+++ init.rb
@@ -1,23 +1,29 @@
-Redmine::Plugin.register :redmine_traceability do
-  name :'Traceability Matrix'
-  author 'Emergya Consultoría'
-  description :'traceability.plugin_description'
-  version '1.0'
+require 'redmine'
 
-  settings :default => {'tracker0' => nil, 'tracker1' => nil},
-           :partial => 'settings/settings'
+Rails.configuration.after_initialize do
+  I18n.with_locale(Setting.default_language) do
+    Redmine::Plugin.register :redmine_traceability do
+      name I18n.t :'Traceability Matrix'
+      description I18n.t :'traceability.plugin_description'
+      author 'Emergya'
+      version '1.0'
 
-  # This plugin adds a project module
-  # It can be enabled/disabled at project level (Project settings -> Modules)
-  project_module :traceability do
-    # This permission has to be explicitly given
-    # It will be listed on the permissions screen
-    permission :view_mt, {:mt => [:index]}
+      settings :default => {'tracker0' => nil, 'tracker1' => nil},
+      :partial => 'settings/settings'
+
+      # This plugin adds a project module
+      # It can be enabled/disabled at project level (Project settings -> Modules)
+      project_module :traceability do
+        # This permission has to be explicitly given
+        # It will be listed on the permissions screen
+        permission :view_mt, {:mt => [:index]}
+      end
+
+      # A new item is added to the project menu
+      menu :project_menu, :traceability,
+      {:controller => 'mt', :action => 'index'},
+      :caption => :'traceability.name',
+      :param => :project_id
+    end
   end
-
-  # A new item is added to the project menu
-  menu :project_menu, :traceability,
-    {:controller => 'mt', :action => 'index'},
-    :caption => :'traceability.name',
-    :param => :project_id
 end
