20c56866cbdcac80d434316a1091edfb00359178
Jose M. Prieto <jmprieto@emergya.es>
2012-03-28 12:53:10 +0200

    rename trackers, add a new intermediate tracker

--- app/controllers/mt_controller.rb
+++ app/controllers/mt_controller.rb
@@ -5,23 +5,23 @@ class MtController < ApplicationController
   menu_item :traceability
 
   def index
-    issues = @tracker0.issues.find(:all,
+    issues = @tracker_rows.issues.find(:all,
       :conditions => ['project_id=?', @project.id],
       :order => 'id')
 
-    @issue_pairs = tracker1_seen_issues = []
+    @issue_pairs = tracker_cols_seen_issues = []
     issues.each do |issue|
       rel_issues_collection = []
       issue.relations_to.each do |issue_relation|
         rel_issue = issue_relation.issue_from
-        if rel_issue.tracker == @tracker1
+        if rel_issue.tracker == @tracker_cols
           rel_issues_collection << rel_issue
         end
       end
 
       issue.relations_from.each do |issue_relation|
         rel_issue = issue_relation.issue_to
-        if rel_issue.tracker == @tracker1
+        if rel_issue.tracker == @tracker_cols
           rel_issues_collection << rel_issue
         end
       end
@@ -29,16 +29,16 @@ class MtController < ApplicationController
       if rel_issues_collection.empty?
         @issue_pairs << [issue, nil]
       else
-        tracker1_seen_issues |= rel_issues_collection
+        tracker_cols_seen_issues |= rel_issues_collection
         rel_issues_collection.sort! { |x, y| x.id <=> y.id }
         @issue_pairs << [issue, rel_issues_collection]
       end
     end
 
-    @tracker1_issues = @tracker1.issues.find(:all,
+    @tracker_cols_issues = @tracker_cols.issues.find(:all,
       :conditions => ['project_id = ?', @project.id],
       :order => 'id')
-    @tracker1_not_seen_issues = @tracker1_issues - tracker1_seen_issues
+    @tracker_cols_not_seen_issues = @tracker_cols_issues - tracker_cols_seen_issues
   end
 
   private
@@ -47,8 +47,9 @@ class MtController < ApplicationController
   end
 
   def get_trackers
-    @tracker0 = Tracker.find(Setting.plugin_redmine_traceability['tracker0'])
-    @tracker1 = Tracker.find(Setting.plugin_redmine_traceability['tracker1'])
+    @tracker_rows = Tracker.find(Setting.plugin_redmine_traceability['tracker0'])
+    @tracker_cols = Tracker.find(Setting.plugin_redmine_traceability['tracker1'])
+    @tracker_int = Tracker.first(Setting.plugin_redmine_traceability['tracker2'])
   rescue ActiveRecord::RecordNotFound
     flash[:error] = l(:'traceability.setup')
     render
