2e35facddf8a3f0b6dd56e1106056507cf3c5002
Jose M. Prieto <jmprieto@emergya.es>
2012-06-01 12:27:41 +0200

    routes: adapt to redmine 2.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-ActionController::Routing::Routes.draw do |map|
-  map.traceability '/projects/:project_id/traceability', :controller => 'mt', :action => 'index'
+if Redmine::VERSION::MAJOR < 2
+  ActionController::Routing::Routes.draw do |map|
+    map.traceability '/projects/:project_id/traceability', :controller => 'mt', :action => 'index'
+  end
+else
+  get '/projects/:project_id/traceability', :to => 'mt#index', :as => :traceability
 end
